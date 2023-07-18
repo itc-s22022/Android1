58d862f4ed03f786765416facfc9a41dc6b30dee
s22022 <s22022@std.it-college.ac.jp>
2023-07-18 15:59:02 +0900

    5.1.5 まで完了

--- ListViewSample/app/src/main/java/ja/ac/it_college/std/s22022/listviewsample/MainActivity.kt
+++ ListViewSample/app/src/main/java/ja/ac/it_college/std/s22022/listviewsample/MainActivity.kt
@@ -1,11 +1,27 @@
 package ja.ac.it_college.std.s22022.listviewsample
 
+import android.graphics.ColorSpace.Adaptation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
+import ja.ac.it_college.std.s22022.listviewsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val item = parent?.getItemAtPosition(position) as String
+            val show = "あなたが選んだ定食: $item"
+            Toast.makeText(this@MainActivity, show, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
