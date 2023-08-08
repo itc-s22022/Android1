2b7ce31152df55c9967077342b77fd4c2fbf56b0
s22022 <s22022@std.it-college.ac.jp>
2023-08-08 16:07:59 +0900

    8.1 までできた。　とりあえず7章 のサンプルと同じ動きがする

--- MenuSample/app/src/main/java/ja/ac/it_college/std/s22022/menusample/MainActivity.kt
+++ MenuSample/app/src/main/java/ja/ac/it_college/std/s22022/menusample/MainActivity.kt
@@ -1,11 +1,33 @@
 package ja.ac.it_college.std.s22022.menusample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ja.ac.it_college.std.s22022.menusample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initList(binding.lvMenu)
+    }
+    private fun initList(view: RecyclerView) {
+        view.adapter = MenuListAdapter(teishokuList, ::order)
+        val layoutManager = LinearLayoutManager(this)
+        view.layoutManager = layoutManager
+        view.addItemDecoration(DividerItemDecoration(this, layoutManager.orientation))
+    }
+
+    private fun order (name: String, price: Int) {
+        startActivity(Intent(this, MenuThanksActivity::class.java).apply {
+            putExtra("menuName", name)
+            putExtra("menuPrice", price)
+        })
     }
 }
