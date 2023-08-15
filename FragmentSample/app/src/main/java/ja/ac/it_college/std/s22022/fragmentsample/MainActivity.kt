97fc9d6f7045d85d6235fabe938df2effd0c2144
s22022 <s22022@std.it-college.ac.jp>
2023-08-15 16:45:35 +0900

    9.3 まで出来た。 教科書との差異↓ ・RecyclerView を使ってる。 ・各Fragmentクラスにはできるだけ外部に依存しないようなコードに変更。 ・Fragment Result API を採用

--- FragmentSample/app/src/main/java/ja/ac/it_college/std/s22022/fragmentsample/MainActivity.kt
+++ FragmentSample/app/src/main/java/ja/ac/it_college/std/s22022/fragmentsample/MainActivity.kt
@@ -2,6 +2,9 @@ package ja.ac.it_college.std.s22022.fragmentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.os.bundleOf
+import androidx.fragment.app.commit
 import ja.ac.it_college.std.s22022.fragmentsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // イベント識別名、LifecycleOwner, イベントリスナ　を渡して待ち受ける
+        supportFragmentManager
+            .setFragmentResultListener(REQUEST_SELECTED_MENU, this, ::onSelectedMenu)
+    }
+
+    private fun onSelectedMenu(requestKey: String, bundle: Bundle) {
+        Log.i("SELECTED_MENU", "requestKey: ${requestKey}, bundle: ${bundle}.")
+        // MenuListFragment から受け取ったデータを詰め直して
+        // MenuThanksFragment を表示させる。
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            addToBackStack("Only List")
+            replace(
+                R.id.fragmentMainContainer, MenuThanksFragment::class.java,
+                bundleOf(
+                    ARG_NAME to bundle.getString(RESULT_NAME, ""),
+                    ARG_PRICE to bundle.getInt(RESULT_PRICE, 0)
+                )
+            )
+        }
     }
 }
--- FragmentSample/app/src/main/java/ja/ac/it_college/std/s22022/fragmentsample/MenuAdapter.kt
+++ FragmentSample/app/src/main/java/ja/ac/it_college/std/s22022/fragmentsample/MenuAdapter.kt
@@ -5,7 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import ja.ac.it_college.std.s22022.fragmentsample.databinding.MenuRowBinding
 
-class MenuAdapter(private val data: List<Menu>) :
+class MenuAdapter(
+    private val data: List<Menu>,
+    private val callback: (Menu) -> Unit
+    ) :
     RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
 
     class ViewHolder(val binding: MenuRowBinding) : RecyclerView.ViewHolder(binding.root)
@@ -16,7 +19,11 @@ class MenuAdapter(private val data: List<Menu>) :
     override fun getItemCount(): Int = data.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.menuName.text = data[position].name
-        holder.binding.menuPrice.text = "%,d".format(data[position].price)
+        val menu = data[position]
+        holder.binding.apply {
+            menuName.text = menu.name
+            menuPrice.text = "%,d".format(menu.price)
+            root.setOnClickListener { callback(menu) }
+        }
     }
 }
