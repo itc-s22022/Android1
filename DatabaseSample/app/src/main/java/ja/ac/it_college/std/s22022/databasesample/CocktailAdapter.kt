406d1b66c7480789529300fad80e3f039c676590
s22022 <s22022@std.it-college.ac.jp>
2023-08-17 16:26:53 +0900

    10.1まで。最初の画面 の表示まで出来た。

---
+++ DatabaseSample/app/src/main/java/ja/ac/it_college/std/s22022/databasesample/CocktailAdapter.kt
@@ -0,0 +1,26 @@
+package ja.ac.it_college.std.s22022.databasesample
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ja.ac.it_college.std.s22022.databasesample.databinding.RowBinding
+
+class CocktailAdapter(
+    private val data: List<String>,
+    private val callback: (Int, String) -> Unit
+    ): RecyclerView.Adapter<CocktailAdapter.ViewHolder> () {
+
+    class ViewHolder(internal val binding: RowBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(RowBinding.inflate(LayoutInflater.from(parent.context), parent, false
+        ))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.tvCocktailName.text = data[position]
+        holder.binding.root.setOnClickListener {
+            callback(position, data[position])
+        }
+    }
+    override fun getItemCount() = data.size
+}
